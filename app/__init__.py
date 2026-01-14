4aa7c446ef153009009da59caef45db70c79fe7d
Nikhil Sathe <nikhilsathe04@gmail.com>
2026-01-14 19:59:42 +0530

    added db and app config

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,12 @@
-<<<<<<< Updated upstream
-from flask import Flask
+from flask import Flask, login_manager
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()                                                         # create database object globally
-=======
-from flask_login import LoginManager
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-login_manager = LoginManager()
->>>>>>> Stashed changes
 
 def create_app():
     app = Flask(__name__)
 
-<<<<<<< Updated upstream
-    app.config['SECRET_KEY'] = 'to_do_or_not_to_do'                       # create a key for sessions
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'           # database configuration
-    app.config['SQLALCHEMY_MODIFICATIONS'] = False                        # currently false to avoid warnings
-
-    db.__init__(app)
-
-    from app.routes.auth import auth_bp                                   # import blue_prints
-    from app.routes.auth import tasks_bp                                  
-    app.register_blueprint(auth_bp)                                       # register blue_prints         
-    app.register_blueprint(tasks_bp)
-    
-    return app
-=======
     app.config['SECRET_KEY'] = 'to_do_or_not_to_do'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -44,4 +21,3 @@ def create_app():
     app.register_blueprint(tasks_bp)
 
     return app
->>>>>>> Stashed changes
